2027a62574586e6037ce6c429b2969501b0b7ef7
Camila Grondona <grondonacamila@gmail.com>
2023-07-26 18:55:31 +0200

    mensaje bienvenida y prompt edad

--- main.js
+++ main.js
@@ -1,29 +1,27 @@
 alert("¡Hola, Bienvenido/a a Perona Servicios de Salud! Para cotizar tu plan, introducí a continuación los datos requeridos:");
 
-
 let nombre = prompt("Ingresá tu nombre y apellido");
 alert(`Un gusto tenerte por acá ${nombre}, solo necesitamos unos datos más para terminar`);
 
-let edad = prompt
+let edad;
 
-    (` Escribí por favor tu rango etario, eligiendo entre estas tres alternativas:
-    Menor de 25
-    Entre 25 y 35 
-    Mayor de 35
-    `);
+do {
+    edad = parseInt(prompt(`Escribí por favor tu rango etario, eligiendo entre estas tres alternativas:
+    1- Menor de 25
+    2- Entre 25 y 35 
+    3- Mayor de 35
+    `));
+} while (isNaN (edad) || edad < 1 || edad > 3);
 
 switch (edad) {
-    case 'Menor de 25'.toLowerCase():
+    case 1: 
         alert("¡Genial, has elegido la opción 1, menor de 25!");
         break;
-    case 'Entre 25 y 35'.toLowerCase():
+    case 2:
         alert("¡Genial, has elegido la opción 2, entre 25 y 35!");
         break; 
-    case 'Mayor de 35'.toLowerCase():
+    case 3:
         alert("¡Genial, has elegido la opción 3, mayor de 35!");
         break; 
-    default:
-        alert("Por favor, selecciona una opción válida");
-        break;
 }
 
